89f10023441fe6796a8595d2483a699f4df982a0
m-marqx <m.marqx@hotmail.com>
2023-04-01 16:58:09 -0300

    breaking change(IRB_Strategy): refactor all functions as class methods

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import model.strategy.IRB
-import model.indicators.moving_average
-import controller.binance_api
+from model.strategy.IRB import IRB_Strategy
+from controller.binance_api import futures_API
+
+fAPI = futures_API()
+BTC = fAPI.get_all_futures_klines_df("BTCUSD_PERP", "2h", 7_200_000)
+
+strategy = IRB_Strategy(BTC)
+strategy = strategy.run_IRB_model(2,20,0.1)
