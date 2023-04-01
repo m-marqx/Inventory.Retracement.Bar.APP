6d9341e9f59984fe8676b6a539452b588da00d53
m-marqx <m.marqx@hotmail.com>
2023-04-01 19:07:09 -0300

    style: format code to conform to PEP8 guidelines

--- model/indicators/moving_average.py
+++ model/indicators/moving_average.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Indicadores
-class moving_average:
+class MovingAverage:
     """
     A class for calculating Simple Moving Average (SMA) 
     and Exponential Moving Average (EMA) of time series data.
@@ -42,15 +42,16 @@ class moving_average:
 
     def ema(self, source: pd.DataFrame, length: int) -> pd.DataFrame:
         """
-        Calculate the Exponential Moving Average (EMA) of the input time series data.
-        
+        Calculate the Exponential Moving Average (EMA) 
+        of the input time series data.
+
         Parameters:
         -----------
         source : pandas.Series
             The time series data to calculate the EMA for.
         length : int
             The number of periods to include in the EMA calculation.
-            
+
         Returns:
         --------
         pandas.Series
@@ -59,5 +60,8 @@ class moving_average:
         sma = source.rolling(window=length, min_periods=length).mean()[:length]
         rest = source[length:]
         return (
-            pd.concat([sma, rest]).ewm(span=length, adjust=False).mean().dropna(axis=0)
+            pd.concat([sma, rest])
+            .ewm(span=length, adjust=False)
+            .mean()
+            .dropna(axis=0)
         )
