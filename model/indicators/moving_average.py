257d90d49fa481a51e5c78c27dc70be68696acbe
m-marqx <m.marqx@hotmail.com>
2023-04-01 17:37:19 -0300

    docs(moving_average): add docstrings to ema method

--- model/indicators/moving_average.py
+++ model/indicators/moving_average.py
@@ -6,7 +6,22 @@ class moving_average:
         sma = source.rolling(length).mean()
         return sma.dropna(axis=0)
 
-    def ema(self, source, length):
+    def ema(self, source: pd.DataFrame, length: int) -> pd.DataFrame:
+        """
+        Calculate the Exponential Moving Average (EMA) of the input time series data.
+        
+        Parameters:
+        -----------
+        source : pandas.Series
+            The time series data to calculate the EMA for.
+        length : int
+            The number of periods to include in the EMA calculation.
+            
+        Returns:
+        --------
+        pandas.Series
+            The calculated EMA time series data.
+        """
         sma = source.rolling(window=length, min_periods=length).mean()[:length]
         rest = source[length:]
         return (
