2cee16b35f60ab0cb6f92697050ad22def708eb8
m-marqx <m.marqx@hotmail.com>
2023-04-01 17:39:52 -0300

    docs: add docstrings to moving_average class

--- model/indicators/moving_average.py
+++ model/indicators/moving_average.py
@@ -2,6 +2,24 @@ import pandas as pd
 
 # Indicadores
 class moving_average:
+    """
+    A class for calculating Simple Moving Average (SMA) 
+    and Exponential Moving Average (EMA) of time series data.
+
+    Attributes:
+    -----------
+    None
+
+    Methods:
+    --------
+    sma(source: pd.DataFrame, length: int) -> pd.DataFrame:
+        Calculate the Simple Moving Average (SMA)
+        of the input time series data.
+
+    ema(source: pd.DataFrame, length: int) -> pd.DataFrame:
+        Calculate the Exponential Moving Average (EMA)
+        of the input time series data.
+    """
     def sma(self, source: pd.DataFrame, length: int) -> pd.DataFrame:
         """
         Calculate the Simple Moving Average (SMA) 
