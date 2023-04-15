1c921382d03ae1c2866e9183b6218fd1474d4c7a
m-marqx <m.marqx@hotmail.com>
2023-04-15 18:10:50 -0300

    feat: add IrbParamsBacktest class

--- model/utils/backtest_params.py
+++ model/utils/backtest_params.py
@@ -8,3 +8,10 @@ class EmaParamsBacktest(BaseModel):
     source_column: List[str] = ["open", "high", "low", "close"]
 
 
+class IrbParamsBacktest(BaseModel):
+    lowestlow: List[int] = np.arange(1, 2, 1)
+    payoff: List[int] = np.arange(2, 3, 1)
+    ticksize: List[float] = np.arange(0.1, 0.2)
+    wick_percentage: List[float] = np.round(np.arange(0.01, 1.01, 0.01), 2)
+
+
