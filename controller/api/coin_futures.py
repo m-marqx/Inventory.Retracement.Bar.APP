f272b23bb203c8d92f006f25561512448cd568e1
m-marqx <124513922+m-marqx@users.noreply.github.com>
2023-04-25 20:06:22 -0300

    refactor: move `get_df_to_csv` to Klines class in utils.py

--- controller/api/coin_futures.py
+++ controller/api/coin_futures.py
@@ -224,29 +224,3 @@ class coin_margined:
         dataframe[int_column] = dataframe[int_column].astype(int)
         dataframe.set_index("open_time", inplace=True)
         return dataframe
-
-    def get_df_to_csv(self, dataframe, name):
-        str_name = f"{name}.csv"
-        columns = [
-            "open",
-            "high",
-            "low",
-            "close",
-            "volume",
-            "close_time",
-            "quote_asset_volume",
-            "number_of_trades",
-            "taker_buy_base_asset_volume",
-            "taker_buy_quote_asset_volume",
-            "ignore",
-        ]
-        dataframe.to_csv(
-            f"model/data/{str_name}",
-            index=True,
-            header=columns,
-            sep=";",
-            decimal=".",
-            encoding="utf-8",
-        )
-
-        return print(str_name + " has been saved")
--- controller/api/utils.py
+++ controller/api/utils.py
@@ -3,3 +3,29 @@ import pandas as pd
 class Klines:
     def __init__(self, klines_list):
         self.klines_list = klines_list
+
+    def get_df_to_csv(self, dataframe, name) -> None:
+        str_name = f"{name}.csv"
+        columns = [
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+            "close_time",
+            "quote_asset_volume",
+            "number_of_trades",
+            "taker_buy_base_asset_volume",
+            "taker_buy_quote_asset_volume",
+            "ignore",
+        ]
+        dataframe.to_csv(
+            f"model/data/{str_name}",
+            index=True,
+            header=columns,
+            sep=";",
+            decimal=".",
+            encoding="utf-8",
+        )
+
+        return print(str_name + " has been saved")
