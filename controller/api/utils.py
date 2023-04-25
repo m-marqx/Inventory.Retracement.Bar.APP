ea361bc76c2c79af7d59b6428bb749e67bab09a8
m-marqx <124513922+m-marqx@users.noreply.github.com>
2023-04-25 20:06:57 -0300

    feat(Klines): add klines_df method

--- controller/api/utils.py
+++ controller/api/utils.py
@@ -4,6 +4,43 @@ class Klines:
     def __init__(self, klines_list):
         self.klines_list = klines_list
 
+    def klines_df(self) -> pd.DataFrame:
+        timestamp = ["open_time", "close_time"]
+
+        float_column = [
+            "open",
+            "high",
+            "low",
+            "close",
+            "quote_asset_volume",
+            "taker_buy_quote_asset_volume",
+        ]
+
+        int_column = ["volume", "number_of_trades", "taker_buy_base_asset_volume"]
+
+        columns = (
+            "open_time",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+            "close_time",
+            "quote_asset_volume",
+            "number_of_trades",
+            "taker_buy_base_asset_volume",
+            "taker_buy_quote_asset_volume",
+            "ignore",
+        )
+
+        dataframe = pd.DataFrame(self.klines_list, columns=columns)
+
+        dataframe[timestamp] = dataframe[timestamp].astype("datetime64[ms]")
+        dataframe[float_column] = dataframe[float_column].astype(float)
+        dataframe[int_column] = dataframe[int_column].astype(int)
+        dataframe.set_index("open_time", inplace=True)
+        return dataframe
+
     def get_df_to_csv(self, dataframe, name) -> None:
         str_name = f"{name}.csv"
         columns = [
