98426a987dfe1b79628527891db4d3cb5df0dd87
m-marqx <124513922+m-marqx@users.noreply.github.com>
2023-05-06 11:27:38 -0300

    refactor(get_All_Klines): implement spot klines calculation

--- controller/api/coin_futures.py
+++ controller/api/coin_futures.py
@@ -76,15 +76,29 @@ class CoinMargined:
 
     def get_All_Klines(
         self,
-        start_time=1597118400000,
+        start_time=1502942400000,
         klines_function="futures",
     ):
 
+        klines_function = klines_function.lower()
+        if (
+            (klines_function == "futures" or klines_function == "mark price")
+            and start_time < 1597118400000
+        ):
+
+            start_time = 1597118400000
+
         if klines_function == "futures":
             klines_function = self.futures_Kline
-        else:
+        elif klines_function == "mark price":
             klines_function = self.markPrice_futures_Kline
-
+        elif klines_function == "spot":
+            klines_function = self.spot_Kline
+        else:
+            raise TypeError(
+                "Klines function should be either"
+                "'futures', 'mark price' or 'spot'"
+            )
         klines_list = []
         end_times = self.utils.get_end_times(start_time)
 
@@ -108,9 +122,9 @@ class CoinMargined:
         dataframe_path = data_path.joinpath(data_name)
         data_frame = pd.read_parquet(dataframe_path)
         last_time = data_frame["open_time_ms"][-1]
-        new_data = self.get_All_Klines(last_time) #Returns a list with all klines without columns names
+        new_data = self.get_All_Klines(last_time)
         new_dataframe = Klines(new_data).klines_df().reindex(columns=data_frame.columns)
-        old_dataframe = data_frame.iloc[:-1,:] #Remove last row
+        old_dataframe = data_frame.iloc[:-1,:]
         refresh_dataframe = pd.concat([old_dataframe,new_dataframe])
         return refresh_dataframe
 
