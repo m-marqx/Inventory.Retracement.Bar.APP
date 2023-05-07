8996b5f38a8f71920a34471ba6f344dd12afec5b
m-marqx <124513922+m-marqx@users.noreply.github.com>
2023-05-07 20:21:10 -0300

    fix(fig_layout): fix pair name

--- view/dashboard/graph.py
+++ view/dashboard/graph.py
@@ -1,24 +1,44 @@
 import pandas as pd
 import plotly.express as px
-
+from controller.api.klines_api import KlineAPI
 
 class GraphLayout:
-    def __init__(self, data_frame: pd.DataFrame):
+    def __init__(
+        self,
+        data_frame: pd.DataFrame,
+        symbol: str,
+        interval: str,
+        api: str,
+    ):
         self.data_frame = data_frame
         self.tranp_color = "rgba(0,0,0,0)"
         self.title_color = "rgba(255,255,255,0.85)"
         self.label_color = "rgba(255,255,255,0.65)"
         self.primary_color = "#8bbb11"
         self.grid_color = "#595959"
+        self.symbol = symbol
+        self.interval = interval
+        self.api = api
 
 
-    def fig_layout(self, fig, column, symbol: str, interval: str):
+    def fig_layout(self, fig, column):
         ticks = self.data_frame[column].std() / 2
+
+        #In the pair names, the "mark_price" has the same values as the "coin_margined".
+        if self.api == "mark_price":
+            self.api = "coin_margined"
+
+        kline_api = KlineAPI(self.symbol,self.interval,self.api)
+        symbol_info = kline_api.get_exchange_symbol_info()
+        coin_name = list(symbol_info.baseAsset)[0]
+        currency_name = list(symbol_info.quoteAsset)[0]
+        pair_name = f"{coin_name}/{currency_name}"
+
         fig.update_layout(
             paper_bgcolor=self.tranp_color,
             plot_bgcolor=self.tranp_color,
             title={
-                "text": f"{symbol[:3]}/{symbol[-3:]} - {interval}",
+                "text": f"{pair_name} - {self.interval}",
                 "x": 0.5,
                 "font": {"color": self.title_color},
             },
@@ -45,7 +65,7 @@ class GraphLayout:
                 exponentformat="none",
                 dtick=ticks,
                 title={
-                    "text": f"{symbol[-3:]}",
+                    "text": f"{currency_name}",
                     "font": {"color": self.label_color},
                 },
                 color=self.title_color
@@ -53,14 +73,14 @@ class GraphLayout:
         )
         return fig
 
-    def plot_cumulative_results(self, symbol: str, interval: str):
+    def plot_cumulative_results(self):
         column = "Cumulative_Result"
         fig = px.line(x=self.data_frame.index, y=self.data_frame[column], color_discrete_sequence=[self.primary_color])
-        self.fig_layout(fig, column, symbol, interval)
+        self.fig_layout(fig, column)
         return fig
 
-    def plot_close(self, symbol: str, interval: str):
+    def plot_close(self):
         column = "close"
         fig = px.line(x=self.data_frame.index, y=self.data_frame[column], color_discrete_sequence=[self.primary_color])
-        self.fig_layout(fig, column, symbol, interval)
+        self.fig_layout(fig, column)
         return fig
