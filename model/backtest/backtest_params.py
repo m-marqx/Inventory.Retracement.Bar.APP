b41d0be49069234f762bfbf5bfe60b5a4600a622
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-05-14 22:12:34 -0300

    refactor: use range instead of np.arange for integer range values

--- model/backtest/backtest_params.py
+++ model/backtest/backtest_params.py
@@ -4,21 +4,21 @@ from typing import List
 
 
 class EmaParamsBacktest(BaseModel):
-    length: List[int] = np.arange(1, 100 + 1, 1)
+    length: List[int] = range(1, 100 + 1, 1)
     source_column: List[str] = ["open", "high", "low", "close"]
 
 
 class IrbParamsBacktest(BaseModel):
-    lowestlow: List[int] = np.arange(1, 2, 1)
-    payoff: List[int] = np.arange(2, 3, 1)
+    lowestlow: List[int] = range(1, 2, 1)
+    payoff: List[int] = range(2, 3, 1)
     ticksize: List[float] = np.arange(0.1, 0.2)
     wick_percentage: List[float] = np.round(np.arange(0.01, 1.01, 0.01), 2)
 
 
 class IndicatorsParamsBacktest(BaseModel):
     ema_column: List[str] = ["open", "high", "low", "close"]
-    macd_histogram_trend_value: List[int] = np.arange(0, 1, 1)
-    cci_trend_value: List[int] = np.arange(0, 1, 1)
+    macd_histogram_trend_value: List[int] = range(0, 1, 1)
+    cci_trend_value: List[int] = range(0, 1, 1)
 
 
 class TrendParamsBacktest(BaseModel):
@@ -50,4 +50,3 @@ class BacktestParams(BaseModel):
             * len(self.trend_params["cci"])
         )
         return num_combinations
-
