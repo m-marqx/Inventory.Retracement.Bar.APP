3833e6455b01cf14f99ddbc37f35848c33bfcb40
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-05-27 00:19:50 -0300

    feat(MovingAverage): add sema method

--- model/indicators/moving_average.py
+++ model/indicators/moving_average.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Indicadores
+
 class MovingAverage:
     """
     A class for calculating Simple Moving Average (SMA)
@@ -18,6 +18,10 @@ class MovingAverage:
 
     ema(source: pd.Series, length: int) -> pd.Series:
         Calculate the Exponential Moving Average (EMA)
+        of the input time series data.
+
+    sema(source: pd.Series, length: int, smooth: int) -> pd.Series:
+        Calculate the Smoothed Exponential Moving Average (SEMA)
         of the input time series data.
     """
 
@@ -66,3 +70,39 @@ class MovingAverage:
             .mean()
             .dropna(axis=0)
         )
+
+    def sema(self, source: pd.Series, length: int, smooth: int) -> pd.Series:
+        """
+        Calculate the Smoothed Exponential Moving Average (SEMA)
+        of the input time series data.
+
+        Parameters:
+        -----------
+        source : pandas.Series
+            The time series data to calculate the SEMA for.
+        length : int
+            The number of periods to include in the SEMA calculation.
+        smooth : int
+            The smooth of EMAs to calculate.
+
+        Returns:
+        --------
+        pandas.Series
+            The calculeted SEMA time series data.
+        """
+
+        emas_dict = {}
+        emas_dict["source_1"] = self.ema(source, length)
+        for value in range(2, smooth + 1):
+            emas_dict[f"source_{value}"] = self.ema(
+                emas_dict[f"source_{value-1}"],
+                length,
+            )
+        emas_df = pd.DataFrame(emas_dict)
+        emas_df["sema"] = (
+            emas_df[emas_df.columns[:-1]].diff(axis=1).sum(axis=1) * - 1
+            * smooth
+            + emas_df[emas_df.columns[-1]]
+        )
+        sema = emas_df["sema"]
+        return sema.dropna(axis=0)
