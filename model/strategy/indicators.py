332ef1aebb906d455622dbfec65ae898ccdce993
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-05-31 22:56:57 -0300

    docs(CalculateCCI): add docstring

--- model/strategy/indicators.py
+++ model/strategy/indicators.py
@@ -72,13 +72,32 @@ class CalculateMACD(BaseStrategy):
         self.df_filtered["MACD_Histogram"] = self.histogram
         return self.df_filtered
 
+
 class CalculateCCI(BaseStrategy):
     def __init__(self, dataframe, params: CCIParams):
+        """
+        Initialize the CalculateCCI object.
+
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            The input dataframe.
+        params : CCIParams
+            The parameters for CCI calculation.
+        """
         super().__init__(dataframe)
         self.source = self.df_filtered[params.source_column]
         self.params = params
 
     def execute(self):
+        """
+        Execute the CCI calculation.
+
+        Returns:
+        --------
+        pd.DataFrame
+            The dataframe with CCI values.
+        """
         from model.indicators.CCI import CCI
 
         self.CCI = CCI(self.source, self.params.length)
