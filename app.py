59d34e416679f254f1f432f31fa9fc4501edf22c
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-06-04 18:34:11 -0300

    refactor: remove unused imports

--- app.py
+++ app.py
@@ -1,29 +1,4 @@
-import pathlib
-import dash
-from dash import Input, Output, State
-import pandas as pd
-import numpy as np
-from controller.api.klines_api import KlineAPI
-from model.strategy.params import (
-    EmaParams,
-    MACDParams,
-    CCIParams,
-    TrendParams,
-    IrbParams,
-    IndicatorsParams,
-)
-
-from model.backtest import Backtest, BacktestParams
-from model.utils.utils import SaveDataFrame, DataProcess
-
 from view.dashboard.layout import app
-from view.dashboard.graph import GraphLayout
-from view.dashboard.utils import (
-    BuilderParams,
-    get_data,
-    builder,
-)
-
 from view.dashboard.pages.lang.lang_callbacks import LangCallbacks
 from view.dashboard.pages.home.label_callbacks import LabelCallbacks
 from view.dashboard.pages.home.strategy_callback import RunStrategy
