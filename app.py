1c13a1234a08630bbe986838218b8c077ca5de20
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-06-04 22:26:00 -0300

    refactor: create init.py file and update import in app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from view.dashboard.layout import app
-from view.dashboard.pages.lang.lang_callbacks import LangCallbacks
-from view.dashboard.pages.home.label_callbacks import LabelCallbacks
-from view.dashboard.pages.home.strategy_callback import RunStrategy
-from view.dashboard.pages.backtest.backtest_callback import RunBacktest
-from view.dashboard.pages.backtest.backtest_params_callback import BacktestParams
-from view.dashboard.pages.general.general_collapse_callbacks import GeneralCollapse
-from view.dashboard.pages.general.nav_link_pages_callbacks import NavLinkPages
+from view.dashboard.pages import (
+    LangCallbacks,
+    LabelCallbacks,
+    RunStrategy,
+    RunBacktest,
+    BacktestParams,
+    GeneralCollapse,
+    NavLinkPages,
+)
 
 server = app.server
 
---
+++ view/dashboard/pages/__init__.py
@@ -0,0 +1,7 @@
+from .lang.lang_callbacks import LangCallbacks
+from .home.label_callbacks import LabelCallbacks
+from .home.strategy_callback import RunStrategy
+from .backtest.backtest_callback import RunBacktest
+from .backtest.backtest_params_callback import BacktestParams
+from .general.general_collapse_callbacks import GeneralCollapse
+from .general.nav_link_pages_callbacks import NavLinkPages
