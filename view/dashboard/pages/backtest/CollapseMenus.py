560b05142876946ff7569e98418e7c4b393a5118
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-06-23 17:13:44 -0300

    feat(BacktestMenuCollapse): add parameters_component

--- view/dashboard/pages/backtest/CollapseMenus.py
+++ view/dashboard/pages/backtest/CollapseMenus.py
@@ -21,3 +21,25 @@ class BacktestMenuCollapse:
             id_prefix="get_data",
             is_open=True,
         ).component
+
+    @property
+    def parameters_component(self):
+        indicators_parameters_component = dbc.Row(
+            [
+                dbc.Col(
+                    self.backtest_components.indicators_parameters_col1,
+                    width=6,
+                ),
+                dbc.Col(
+                    self.backtest_components.indicators_parameters_col2,
+                    width=6,
+                ),
+            ]
+        )
+
+        return MenuCollapse(
+            lang=self.lang,
+            label="MODIFY_INDICATORS_PARAMETERS_BUTTON",
+            component=indicators_parameters_component,
+            id_prefix="indicator_params",
+        ).component
