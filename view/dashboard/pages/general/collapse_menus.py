9e62ad9f0f891a51d1cc07b39bdf508ed8467218
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-06-25 22:45:08 -0300

    feat(LayoutMenuCollapse): add strategy_component method

--- view/dashboard/pages/general/collapse_menus.py
+++ view/dashboard/pages/general/collapse_menus.py
@@ -49,3 +49,34 @@ class LayoutMenuCollapse:
             component=indicators_parameters_component,
             id_prefix="indicator_params",
         ).simple_collapse
+
+    @property
+    def strategy_component(self):
+        irb_parameters = dbc.Row(
+            [
+                dbc.Col(
+                    self.page_component_base.irb_parameters_col1,
+                    style={
+                        "display": "flex",
+                        "flex-direction": "column",
+                    },
+                    width=6,
+                ),
+                dbc.Col(
+                    self.page_component_base.irb_parameters_col2,
+                    style={
+                        "display": "flex",
+                        "flex-direction": "column",
+                    },
+                    width=6,
+                ),
+            ]
+        )
+
+        return MenuCollapse(
+            lang=self.lang,
+            label="MODIFY_STRATEGY_PARAMETERS_BUTTON",
+            component=irb_parameters,
+            id_prefix="strategy_params",
+        ).simple_collapse
+
