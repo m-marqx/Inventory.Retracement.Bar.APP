c456f885fb4e48399bec1f9650113f4293e7faa3
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-06-25 22:44:53 -0300

    feat(LayoutMenuCollapse): add parameters_component method

--- view/dashboard/pages/general/collapse_menus.py
+++ view/dashboard/pages/general/collapse_menus.py
@@ -27,3 +27,25 @@ class LayoutMenuCollapse:
             id_prefix="get_data",
             is_open=True,
         ).simple_collapse
+
+    @property
+    def parameters_component(self):
+        indicators_parameters_component = dbc.Row(
+            [
+                dbc.Col(
+                    self.page_component_base.indicators_parameters_col1,
+                    width=6,
+                ),
+                dbc.Col(
+                    self.page_component_base.indicators_parameters_col2,
+                    width=6,
+                ),
+            ]
+        )
+
+        return MenuCollapse(
+            lang=self.lang,
+            label="MODIFY_INDICATORS_PARAMETERS_BUTTON",
+            component=indicators_parameters_component,
+            id_prefix="indicator_params",
+        ).simple_collapse
