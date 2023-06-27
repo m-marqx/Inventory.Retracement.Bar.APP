dec44f727020a51dc35412bc2053983e8b899ca9
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-06-27 20:50:34 -0300

    docs(Backtest): add docstring

--- model/backtest/backtest.py
+++ model/backtest/backtest.py
@@ -20,10 +20,49 @@ from view.dashboard.utils import (
     builder,
 )
 
-class Backtest:
-    def __init__(self, dataframe: pd.DataFrame, hardware_type: str = "GPU"):
-        self.dataframe = dataframe.copy()
 
+class Backtest:
+    """
+    A class for performing backtesting on a given dataframe.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The input dataframe for backtesting.
+    hardware_type : str, optional
+        The type of hardware to use for backtesting (default is "GPU").
+
+    Attributes
+    ----------
+    dataframe : pd.DataFrame
+        The input dataframe for backtesting.
+    strategy_df : pd.DataFrame
+        The dataframe storing the backtesting strategy results.
+    parameters_list : list
+        A list of strategy parameters.
+
+    Methods
+    -------
+    strategy(ema_parameters, irb_parameters, indicator_parameters, trend_parameters, result_parameters)
+        Run the backtesting strategy with the specified parameters.
+    run_backtest(ema_params, irb_params, indicators, trend, result_params, **kwargs)
+        Run a single backtest with the specified parameters.
+    param_grid_backtest(column, n_jobs, params, n_gpu, n_workers_per_gpu)
+        Run backtests with a parameter grid and return the results.
+    """
+
+    def __init__(self, dataframe: pd.DataFrame, hardware_type: str = "GPU"):
+        """
+        Initialize a Backtest instance.
+
+        Parameters
+        ----------
+        dataframe : pd.DataFrame
+            The input dataframe for backtesting.
+        hardware_type : str, optional
+            The type of hardware to use for backtesting (default is "GPU").
+        """
+        self.dataframe = dataframe.copy()
         self.strategy_df = pd.DataFrame()
         self.parameters_list = []
 
@@ -40,6 +79,28 @@ class Backtest:
         trend_parameters: TrendParams,
         result_parameters: ResultParams,
     ):
+        """
+        Run the backtesting strategy with the specified parameters.
+
+        Parameters
+        ----------
+        ema_parameters : EmaParams
+            The parameters for the EMA strategy.
+        irb_parameters : IrbParams
+            The parameters for the IRB strategy.
+        indicator_parameters : IndicatorsParams
+            The parameters for the indicator strategy.
+        trend_parameters : TrendParams
+            The parameters for the trend strategy.
+        result_parameters : ResultParams
+            The parameters for the result strategy.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the strategy dataframe and the parameters
+            list.
+        """
         self.parameters_list = [
             f"EMA: {ema_parameters} <br> "
             f"IRB: {irb_parameters} <br> "
@@ -69,6 +130,28 @@ class Backtest:
         result_params,
         **kwargs
     ):
+        """
+        Run a single backtest with the specified parameters.
+
+        Parameters
+        ----------
+        ema_params : dict
+            The parameters for the EMA strategy.
+        irb_params : dict
+            The parameters for the IRB strategy.
+        indicators : dict
+            The parameters for the indicator strategy.
+        trend : dict
+            The parameters for the trend strategy.
+        result_params : dict
+            The parameters for the result strategy.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the strategy dataframe and the parameters
+            list.
+        """
         return self.strategy(
             ema_params,
             irb_params,
@@ -85,6 +168,31 @@ class Backtest:
         n_gpu=1,
         n_workers_per_gpu=4,
     ):
+        """
+        Run backtests with a parameter grid and return the results.
+
+        Parameters
+        ----------
+        column : str, optional
+            The column to use for the results (default is "Capital").
+        n_jobs : int, optional
+            The number of parallel jobs to run (-1 means using all
+            available processors, default is -1).
+        params : BacktestParams, optional
+            The parameters for the backtest
+            (default is BacktestParams()).
+        n_gpu : int, optional
+            The number of GPUs to use for parallelization
+            (default is 1).
+        n_workers_per_gpu : int, optional
+            The number of workers per GPU
+            (default is 4).
+
+        Returns
+        -------
+        pd.DataFrame
+            A dataframe containing the backtest results.
+        """
         param_grid = {
             'ema_params': ParameterGrid(dict(params.ema_params)),
             'irb_params': ParameterGrid(dict(params.irb_params)),
@@ -94,7 +202,13 @@ class Backtest:
         }
 
         if self.hardware == "GPU":
-            parallelizer = gpuparallel.GPUParallel(n_gpu, n_workers_per_gpu, progressbar=False)
+
+            parallelizer = gpuparallel.GPUParallel(
+                n_gpu,
+                n_workers_per_gpu,
+                progressbar=False
+            )
+
             delayed_strategy = gpuparallel.delayed(self.run_backtest)
         else:
             parallelizer = Parallel(n_jobs)
