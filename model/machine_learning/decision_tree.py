84a28c74209a016eb0b71e6de3c13829eb79fd7d
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-07-03 22:41:40 -0300

    feat(DecisionTreeClassifier): add create_variables method

--- model/machine_learning/decision_tree.py
+++ model/machine_learning/decision_tree.py
@@ -42,3 +42,22 @@ class DecisionTreeClassifier:
 
     def get_proportion(self, column):
         return self.data_frame[column].value_counts(normalize=True) * 100
+
+    def create_variables(self):
+        self.data_frame["std5"] = self.data_frame["Close"].rolling(5).std()
+        self.data_frame["std10"] = self.data_frame["Close"].rolling(10).std()
+        self.data_frame["prop"] = (
+            self.data_frame["Close"] - self.data_frame["Open"]
+        ) / (self.data_frame["High"] - self.data_frame["Low"])
+        self.data_frame["dir_D"] = np.where(
+            self.data_frame["Close"] > self.data_frame["Open"], "1", "0"
+        )
+        self.data_frame["dir_D-1"] = self.data_frame["dir_D"].shift(1)
+        self.data_frame["dir_D-2"] = self.data_frame["dir_D"].shift(2)
+        self.data_frame["dir_D-3"] = self.data_frame["dir_D"].shift(3)
+
+        self.data_frame.dropna(
+            axis=0, how="any", subset=self.data_frame.columns[-7:], inplace=True
+        )
+
+        return self
