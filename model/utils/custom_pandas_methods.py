db31fcc3a98ac0ecf7abb63a79c74f7a5e9def6a
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-07-16 20:05:23 -0300

    feat: add ResampleOHLC class

--- model/utils/custom_pandas_methods.py
+++ model/utils/custom_pandas_methods.py
@@ -52,3 +52,93 @@ def reorder_columns(
     return self[columns_adjusted]
 
 pd.DataFrame.reorder_columns = reorder_columns
+
+class ResampleOHLC(pd.DataFrame):
+    """
+    A subclass of pd.DataFrame for resampling OHLC
+    (open, high, low, close) data.
+
+    Parameters
+    ----------
+    period : str or DateOffset
+        The resampling period for the OHLC data.
+    *args
+        Positional arguments to be passed to the pd.DataFrame
+        constructor.
+    **kwargs
+        Keyword arguments to be passed to the pd.DataFrame
+        constructor.
+
+    Attributes
+    ----------
+    period : str or DateOffset
+        The resampling period for the OHLC data.
+
+    Methods
+    -------
+    ohlc()
+        Resamples the data and returns the OHLC values.
+    OHLC()
+        Resamples the data and returns the OHLC values
+        (alternative column names).
+
+    Notes
+    -----
+    The Klines values can be different from Binance when compared,
+    especially when the Klines timeframe is too short, for example,
+    `1 minute x 2 hours`. This is because Binance only considers
+    the opening opening price when the candle has at least one trade.
+    If a candle doesn't have at least one trade, that price doesn't
+    exist for it.
+
+    """
+
+    def __init__(self, period, *args, **kwargs):
+        """
+        Initialize the ResampleOHLC class.
+
+        Parameters
+        ----------
+        period : str or DateOffset
+            The resampling period for the OHLC data.
+        *args
+            Positional arguments to be passed to the pd.DataFrame
+            constructor.
+        **kwargs
+            Keyword arguments to be passed to the pd.DataFrame
+            constructor.
+
+        """
+        super().__init__(*args, **kwargs)
+        self.period = period
+
+    def ohlc(self):
+        """
+        Resamples the data and returns the OHLC values.
+
+        Returns
+        -------
+        pd.DataFrame
+            The resampled data with OHLC values.
+
+        Raises
+        ------
+        ValueError
+            If no OHLC columns are found in the data.
+
+        """
+        ohlc = all(
+            column in self.columns
+            for column in ["open", "high", "low", "close"]
+        )
+
+        if ohlc:
+            resampled = self.resample(self.period).agg({
+                'open': 'first',
+                'high': 'max',
+                'low': 'min',
+                'close': 'last'
+            })
+            return resampled.dropna()
+        raise ValueError("No OHLC columns found")
+
