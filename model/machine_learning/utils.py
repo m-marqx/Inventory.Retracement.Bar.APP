d4b4f97a554db2d17cecba0e975f094a1bf53ff8
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-08-02 21:04:17 -0300

    feat(DataPreprocessor): add get_target_bin method

--- model/machine_learning/utils.py
+++ model/machine_learning/utils.py
@@ -60,3 +60,51 @@ class DataPreprocessor:
             features = ["IRB_Condition", "Signal", "uptrend"]
 
         self.features = list(features)
+
+    def get_target_bin(
+        self,
+        periods: int = 12,
+        column: str = "close",
+        cutoff: float | list[float] = None,
+    ) -> pd.DataFrame:
+        """
+        Generate a target binary DataFrame based on a specified column.
+
+        Parameters:
+        -----------
+        periods : int, optional
+            The number of periods used to calculate the percentage
+            change.
+            (default: 12)
+        column : str, optional
+            The column name from which the target binary is generated.
+            (default: "close")
+        cutoff : float or list of float, optional
+            The cutoff value(s) to determine the target binary values.
+            If a list of float values is provided, multiple target bins
+            will be generated.
+            If None, defaults to [0.25, 0.30, 0.35]
+
+        Returns:
+        --------
+        pd.DataFrame
+            The DataFrame with binary targets.
+        """
+        if cutoff is None:
+            cutoff = [0.25, 0.30, 0.35]
+
+        features = [column] + list(self.features)
+        data_frame = self.dataframe[features].astype("float64").copy()
+
+        data_frame["return"] = data_frame[column].pct_change(periods)
+        data_frame["target"] = data_frame["return"].shift(-periods)
+
+        for value in enumerate(cutoff):
+            cutoff_value = data_frame["target"].std() * value[1]
+            data_frame[f"Target_Bin{value[0]}"] = np.where(
+                data_frame["target"] > cutoff_value,
+                1,
+                np.where(data_frame["target"] < -cutoff_value, -1, 0),
+            )
+
+        return data_frame
