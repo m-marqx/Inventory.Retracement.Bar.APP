4217dc92234bb8da59d16a9df644ed017b73154e
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-08-26 17:55:42 -0300

    lang(en_US): update MODIFY_RESULT_CONFIGS_BUTTON translation

--- view/dashboard/pages/lang/en_US.py
+++ view/dashboard/pages/lang/en_US.py
@@ -80,7 +80,7 @@ en_US = {
     "PLOT_TYPE": "Plot",
     "ALL_LINES": "All Lines",
     "ONLY_POSITIVES": "Positive Lines",
-    "MODIFY_RESULT_CONFIGS_BUTTON" : "Modify Result Configuration",
+    "MODIFY_RESULT_CONFIGS_BUTTON" : "Modify Result Settings",
     # Backtest Button
     "RUN_BACKTEST": "Run Backtest",
 }
