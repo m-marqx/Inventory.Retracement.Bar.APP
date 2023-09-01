37c3e5776aaf2f7aa275b406a0ca5dcd1df73ede
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-09-01 18:13:49 -0300

    docs(MACD): update docstring

--- model/indicators/MACD.py
+++ model/indicators/MACD.py
@@ -41,6 +41,31 @@ class MACD:
         signal_length: int,
         method: Literal["ema", "sma"] = "ema",
     ):
+        """
+        Initialize the Moving Average Convergence Divergence (MACD)
+        indicator.
+
+        Parameters:
+        -----------
+        source : pd.Series
+            The input time series data for calculating MACD.
+        fast_length : int
+            The number of periods for the fast moving average.
+        slow_length : int
+            The number of periods for the slow moving average.
+        signal_length : int
+            The number of periods for the signal line moving average.
+        method : Literal["ema", "sma"], optional
+            The method to use for calculating moving averages, either
+            "ema" for Exponential Moving Average or "sma" for Simple
+            Moving Average.
+            (default: "ema")
+
+        Raises:
+        -------
+        ValueError
+            If an invalid method is provided.
+        """
         self.source = source
         self.fast_length = fast_length
         self.slow_length = slow_length
@@ -53,24 +78,22 @@ class MACD:
             raise ValueError(f"'{method}' is not a valid method.")
 
     def __set_ema(self):
-        """Calculate the fast and slow exponential moving averages."""
         self.fast_ma = ma.ema(self.source, self.fast_length)
         self.slow_ma = ma.ema(self.source, self.slow_length)
 
     def __set_sma(self):
-        """Calculate the fast and slow simple moving averages."""
         self.fast_ma = ma.sma(self.source, self.fast_length)
         self.slow_ma = ma.sma(self.source, self.slow_length)
 
     @property
     def get_histogram(self) -> pd.DataFrame:
-        """Calculate the MACD histogram.
+        """
+        Calculate the MACD histogram.
 
-        Returns
-        -------
-        pandas.DataFrame
-            The MACD histogram.
-
+        Returns:
+        --------
+        pd.DataFrame
+            A DataFrame containing the MACD histogram values.
         """
         macd = (self.fast_ma - self.slow_ma).dropna()
         macd_signal = ma.ema(macd, self.signal_length)
