e626e94a1c25f88c5252ed891f55ba4fddd32e87
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-09-20 22:39:57 -0300

    feat: add ModelHandler class

--- model/machine_learning/utils.py
+++ model/machine_learning/utils.py
@@ -758,9 +758,81 @@ class DataHandler:
                 quantile_df
                 .div(quantile_df.sum(axis=1), axis=0)
             )
-
         return quantile_df
 
+class ModelHandler:
+    """
+    A class for handling machine learning model evaluation.
+
+    Parameters:
+    -----------
+    estimator : object
+        The machine learning model to be evaluated.
+    X_train : array-like of shape (n_samples, n_features)
+        Training input samples.
+    y_train : array-like of shape (n_samples,)
+        Target values for training.
+    X_test : array-like of shape (n_samples, n_features)
+        Testing input samples.
+    y_test : array-like of shape (n_samples,)
+        True target values for testing.
+
+    Attributes:
+    -----------
+    x_train : array-like of shape (n_samples, n_features)
+        Training input samples.
+    y_train : array-like of shape (n_samples,)
+        Target values for training.
+    x_test : array-like of shape (n_samples, n_features)
+        Testing input samples.
+    y_test : array-like of shape (n_samples,)
+        True target values for testing.
+    estimator : object
+        The machine learning model.
+    y_pred_probs : array-like of shape (n_samples,), optional
+        Predicted class probabilities (if available).
+    _has_predic_proba : bool
+        Indicates whether the estimator has predict_proba method.
+
+    Properties:
+    -----------
+    results_report : str
+        A string containing a results report including a confusion matrix,
+        a classification report, AUC, Gini index (if predict_proba is
+        available), and support.
+    """
+    def __init__(self, estimator, X_train, y_train, X_test, y_test) -> None:
+        """
+        Initialize the ModelHandler object.
+
+        Parameters:
+        -----------
+        estimator : object
+            An instance of a scikit-learn estimator for classification or
+            regression.
+        X_train : array-like of shape (n_samples, n_features)
+            Training input samples.
+        y_train : array-like of shape (n_samples,)
+            Target values for training.
+        X_test : array-like of shape (n_samples, n_features)
+            Test input samples.
+        y_test : array-like of shape (n_samples,)
+            True target values for testing.
+        """
+        self.x_train = X_train
+        self.y_train = y_train
+        self.x_test = X_test
+        self.y_test = y_test
+        self.estimator = estimator
+        self.y_pred_probs = None
+        self.y_pred = estimator.predict(X_test)
+        self._has_predic_proba = (
+            hasattr(estimator, 'predict_proba')
+            and callable(getattr(estimator, 'predict_proba'))
+        )
+
+        if self._has_predic_proba:
+            self.y_pred_probs = estimator.predict_proba(X_test)[:, 1]
 class PlotCurve:
     """
     A class for plotting target curves with specified thresholds.
