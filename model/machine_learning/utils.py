715346c5b7b6f5bacd5e33aa00b75409684d14cf
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-11-06 16:21:31 -0300

    dcos(DataHandler): update docstrings

--- model/machine_learning/utils.py
+++ model/machine_learning/utils.py
@@ -351,29 +351,36 @@ class DataHandler:
     """
     Class for handling data preprocessing tasks.
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     dataframe : pd.DataFrame or pd.Series
         The input DataFrame or Series to be processed.
 
-    Attributes:
-    -----------
+    Attributes
+    ----------
     data_frame : pd.DataFrame
         The processed DataFrame.
 
-    Methods:
-    --------
-    drop_zero_predictions(column: str) -> pd.Series
-        Drop rows where the specified column has all zero values.
-
-    get_splits(target: list | str, features: str | list[str])
-    -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]
-        Split the DataFrame into training and testing sets.
-
-    get_best_results(target_column: str) -> pd.DataFrame
-        Get the rows in the DataFrame with the best accuracy for each
-        unique value in the target_column.
-
+    Methods
+    -------
+    get_datasets(feature_columns, test_size=0.5, split_size=0.7)
+        Splits the data into development and validation datasets.
+    drop_zero_predictions(column)
+        Drops rows where the specified column has all zero values.
+    get_splits(target, features)
+        Splits the DataFrame into training and testing sets.
+    get_best_results(target_column)
+        Gets the rows with the best accuracy for each unique value in
+        the target column.
+    result_metrics(result_column=None, is_percentage_data=False,
+    output_format="DataFrame")
+        Calculates result-related statistics like expected return and win rate.
+    fill_outlier(column=None, iqr_scale=1.5, upper_quantile=0.75,
+    down_quantile=0.25)
+        Removes outliers from a specified column using the IQR method.
+    quantile_split(target_input, column=None, method="ratio",
+    quantiles=None, log_values=False)
+        Splits data into quantiles and analyzes the relationship with a target.
     """
 
     def __init__(
