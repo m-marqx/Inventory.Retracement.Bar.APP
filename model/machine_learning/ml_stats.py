9eb7d0a192591ef824bdf2671a7538154a3deebf
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-12-07 22:23:50 -0300

    feat(MLStats): add calculate_return_stats method

--- model/machine_learning/ml_stats.py
+++ model/machine_learning/ml_stats.py
@@ -71,3 +71,41 @@ class MLStats:
         self.data_frame = (max_results - self.data_frame) / max_results
         return self.data_frame.fillna(0).astype("float32")
 
+    def calculate_return_stats(
+        self,
+        reset_dataframe: bool = False
+    ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        """
+        Calculate return statistics.
+
+        Parameters:
+        -----------
+        reset_dataframe : bool, optional
+            Flag to reset the internal DataFrame
+            (default: False).
+
+        Returns:
+        --------
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]
+            Tuple containing positive sum, positive mean, negative sum,
+            and negative mean return statistics.
+        """
+        if self.data_frame.min() > 0:
+            self.data_frame = self.data_frame.diff().fillna()
+
+        rt_sum_pos = self.data_frame[self.data_frame > 0].resample("A").sum()
+        rt_mean_pos = self.data_frame[self.data_frame > 0].resample("A").mean()
+
+        rt_sum_neg = (
+            abs(self.data_frame[self.data_frame < 0]).resample("A").sum()
+        )
+
+        rt_mean_neg = (
+            abs(self.data_frame[self.data_frame < 0]).resample("A").mean()
+        )
+
+        if reset_dataframe:
+            self.data_frame = pd.DataFrame([])
+
+        return rt_sum_pos, rt_mean_pos, rt_sum_neg, rt_mean_neg
+
