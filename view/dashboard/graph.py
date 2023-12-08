723ba1e757200dca9881b405941ef333ab202416
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-12-08 20:52:53 -0300

    docs(GraphLayout): add docstring for class

--- view/dashboard/graph.py
+++ view/dashboard/graph.py
@@ -5,6 +5,39 @@ from controller.api.klines_api import KlineAPI
 
 
 class GraphLayout:
+    """
+    A class for creating interactive line charts and layouts for
+    financial data visualization.
+
+    Methods:
+    --------
+    - __init__(
+        self, data_frame: pd.DataFrame, symbol: str, \
+        interval: str, api: str \
+        ): Constructor method for initializing the GraphLayout class.
+    - fig_layout(self, fig, column): Configure layout settings for the \
+        figure.
+    - custom_fig_layout(self, fig, column): Configure custom layout \
+        settings for the figure.
+    - plot_cumulative_results(self) -> go.Figure: \
+        Plot cumulative results line chart.
+    - plot_single_linechart(self, column) -> go.Figure: \
+        Plot a single line chart for a specified column.
+    - plot_close(self) -> go.Figure: Plot line chart for closing prices.
+    - grouped_lines(self) -> go.Figure: Plot grouped lines for \
+        multiple columns.
+
+    Attributes:
+    -----------
+    - data_frame: pd.DataFrame
+        The financial DataFrame for visualization.
+    - symbol: str
+        The symbol for the financial data.
+    - interval: str
+        The time interval for the financial data.
+    - api: str
+        The API type for the financial data.
+    """
     def __init__(
         self,
         data_frame: pd.DataFrame,
@@ -12,6 +45,20 @@ class GraphLayout:
         interval: str,
         api: str,
     ):
+        """
+        Constructor method for initializing the GraphLayout class.
+
+        Parameters:
+        -----------
+        data_frame : pd.DataFrame
+            The financial DataFrame for visualization.
+        symbol : str
+            The symbol for the financial data.
+        interval : str
+            The time interval for the financial data.
+        api : str
+            The API type for the financial data.
+        """
         self.data_frame = data_frame
         self.tranp_color = "rgba(0,0,0,0)"
         self.title_color = "rgba(255,255,255,0.85)"
