cdd1c1e83b8df19f795852ac54776a2c87593b29
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-01-01 17:53:39 -0300

    fix(app): add all uncalled callbacks

--- app.py
+++ app.py
@@ -9,21 +9,41 @@ from view.dashboard.pages import (
     NavLinkPages,
 )
 
-from view.dashboard.pages.ml.ml_callback import RunModel
+from view.dashboard.pages.dev.ml_callback import DevRunModel
+from view.dashboard.pages.dev.label_callbacks import DevMLLabelCallbacks
+from view.dashboard.pages.dev.ml_collapse_callbacks import DevMLCollapseCallbacks
+
 from view.dashboard.pages.ml.label_callbacks import MLLabelCallbacks
 from view.dashboard.pages.ml.ml_collapse_callbacks import MLCollapseCallbacks
+from view.dashboard.pages.ml.model_ml_callback import ModelMLCallback
+from view.dashboard.pages.ml.inputs_callback import ModelInputsCallbacks
+from view.dashboard.pages.ml.model_params_callback import ModelParamsCallback
 
 server = app.server
 
 if __name__ == "__main__":
-    LabelCallbacks()
+    #General
     LangCallbacks()
+    NavLinkPages()
+    GeneralCollapse()
+
+    #Home
     RunStrategy()
+    LabelCallbacks()
+
+    #Backtest
     RunBacktest()
+    BacktestParams()
+
+    #ML
+    ModelMLCallback()
     MLLabelCallbacks()
     MLCollapseCallbacks()
-    RunModel()
-    BacktestParams()
-    GeneralCollapse()
-    NavLinkPages()
-    app.run(debug=False)
+    ModelInputsCallbacks()
+    ModelParamsCallback()
+
+    #DEV
+    DevRunModel()
+    DevMLLabelCallbacks()
+    DevMLCollapseCallbacks()
+    app.run(debug=True)
